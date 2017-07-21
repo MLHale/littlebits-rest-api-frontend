6d863fc69b0df34b77a2f341396cfdbfc37e0146
Matt Hale <mlhale@unomaha.edu>
2017-07-20 22:53:19 -0500

    small fix for deprecation

--- app/app.js
+++ app/app.js
@@ -5,13 +5,13 @@ import config from './config/environment';
 
 let App;
 
-Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
 });
+
 
 loadInitializers(App, config.modulePrefix);
 
